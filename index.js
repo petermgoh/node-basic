ea86b15a84aa6863aa8c1ba81f6b6509261e3621
petermgoh <45682969+petermgoh@users.noreply.github.com>
2025-01-02 16:31:26 -0600

    Changed routing method
    Utilized url.parse to recreate routing method. Not sure if I like it, as it required mapping of "/" to "./index.html". Express will solve this in the future, I believe.

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const http = require('http');
 const fs = require('fs')
+const url = require('url')
+
 const port = 8080;
 
 http.
     createServer(function (req, res) {
-        switch (req.url) {
-            case '/':
-                page = "index.html"
-                break
-            case '/about':
-                page = "about.html"
-                break
-            case '/contact-me':
-                page = "contact-me.html"
-                break
-            default:
-                page = "404.html"
-        };
-        fs.readFile(page, function(err, data) {
+        let q = url.parse(req.url, true)
+        let filename = q.pathname === "/" ? "./index.html" : "." + q.pathname + ".html"
+        fs.readFile(filename, function(err, data) {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(data);
         });
