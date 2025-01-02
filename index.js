c332a0d758af068eb896fae3a3e90de891f4e3c5
petermgoh <45682969+petermgoh@users.noreply.github.com>
2025-01-02 14:45:58 -0600

    Main Functionality Added
    Added url switch, reads and writes local html files based on url, and added anchor tag links between each file.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const http = require('http');
-
+const fs = require('fs')
 const port = 8080;
 
 http.
     createServer(function (req, res) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end("hello");
+        switch (req.url) {
+            case '/':
+                page = "index.html"
+                break
+            case '/about':
+                page = "about.html"
+                break
+            case '/contact-me':
+                page = "contact-me.html"
+                break
+            default:
+                page = "404.html"
+        };
+        fs.readFile(page, function(err, data) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
     }).listen(8080);
